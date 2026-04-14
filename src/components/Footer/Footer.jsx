4878da4a45e58d182b7197a2ebc61d2c1f94d76c
EngrHenry111 <henryengrakpan@gmail.com>
2026-04-14 15:13:13 +0100

    social media

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -16,10 +16,10 @@ const Footer = () => {
         <p>Global Trade | Currency Exchange | Export Services</p>
 
         <div className="socials">
-          <a href="#"><FaFacebook /></a>
-          <a href="#"><FaInstagram /></a>
+          <a href="https://www.facebook.com/share/18RX9kdkNz/?mibextid=wwXIfr"><FaFacebook /></a>
+          <a href="https://www.instagram.com/yungkingchi?igsh=dDdhb3pybmpueXdi&utm_source=qr"><FaInstagram /></a>
           <a href="#"><FaLinkedin /></a>
-          <a href="#"><FaTiktok /></a>
+          <a href="www.tiktok.com/@kingchivideos"><FaTiktok /></a>
         </div>
 
         <p className="copy">© 2026 All rights reserved</p>
